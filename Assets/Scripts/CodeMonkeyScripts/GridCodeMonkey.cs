68f5d4f79749c4d325f1d6256ad6fc53722cbd5a
Mikzeer <nabil.hallak.nano@gmail.com>
2020-07-10 17:44:16 -0300

    CodeMonkey Grid System Part2 Generics<T>

--- Assets/Scripts/CodeMonkeyScripts/GridCodeMonkey.cs
+++ Assets/Scripts/CodeMonkeyScripts/GridCodeMonkey.cs
@@ -1,42 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridCodeMonkey
+public class GridCodeMonkey<TgridObject>
 {
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
     private int width;
     private int height;
     private float cellSize;
-    private int[,] gridArray;
+    private TgridObject[,] gridArray;
     private Vector3 originPosition;
     private TextMesh[,] debugTextArray;
 
-    public GridCodeMonkey(int width, int height, float cellSize, Vector3 originPosition)
+    public GridCodeMonkey(int width, int height, float cellSize, Vector3 originPosition, Func<GridCodeMonkey<TgridObject>, int, int,TgridObject> createGridObject)
     {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
 
-        gridArray = new int[width, height];
+        gridArray = new TgridObject[width, height];
         debugTextArray = new TextMesh[width, height];
-
-        Debug.Log("width " + width + " height " + height);
+        //Debug.Log("width " + width + " height " + height);
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                gridArray[x, y] = createGridObject(this, x, y);
+            }
+        }
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
             for (int y = 0; y < gridArray.GetLength(1); y++)
             {
-                Debug.Log("x " + x + " y " + y);
-                debugTextArray[x, y] = CreateWorldText(gridArray[x, y].ToString(), Color.white, null, GetWorldPosition(x, y) + new Vector3(cellSize,cellSize) * 0.5f, 40, TextAnchor.MiddleCenter);
+                //Debug.Log("x " + x + " y " + y);
+                debugTextArray[x, y] = CreateWorldText(gridArray[x, y]?.ToString(), Color.white, null, GetWorldPosition(x, y) + new Vector3(cellSize,cellSize) * 0.5f, 40, TextAnchor.MiddleCenter);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
                 Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
         }
         Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) =>
+        {
+            debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y]?.ToString();
+        };
     }
-    
+
     private Vector3 GetWorldPosition(int x , int y)
     {
         return new Vector3(x, y) * cellSize + originPosition;
@@ -48,7 +67,7 @@ public class GridCodeMonkey
         y = Mathf.FloorToInt((worldposition - originPosition).y / cellSize);
     }
 
-    public void SetValue(int x, int y, int value)
+    public void SetGridObject(int x, int y, TgridObject value)
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
@@ -57,14 +76,19 @@ public class GridCodeMonkey
         }        
     }
 
-    public void SetValue(Vector3 worldPosition, int value)
+    public void TriggerGridObjectChanged(int x, int y)
+    {
+        OnGridObjectChanged?.Invoke(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+    }
+
+    public void SetGridObject(Vector3 worldPosition, TgridObject value)
     {
         int x, y;
         GetXY(worldPosition, out x, out y);
-        SetValue(x, y, value);
+        SetGridObject(x, y, value);
     }
 
-    public int GetValue(int x, int y)
+    public TgridObject GetGridObject(int x, int y)
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
@@ -72,15 +96,15 @@ public class GridCodeMonkey
         }
         else
         {
-            return 0;
+            return default(TgridObject);
         }
     }
 
-    public int GetValue(Vector3 worldPosition)
+    public TgridObject GetGridObject(Vector3 worldPosition)
     {
         int x, y;
         GetXY(worldPosition, out x, out y);
-        return GetValue(x, y);
+        return GetGridObject(x, y);
     }
 
     private TextMesh CreateWorldText(string text, Color col, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
--- Assets/Scripts/CodeMonkeyScripts/TestingCodeMonkey.cs
+++ Assets/Scripts/CodeMonkeyScripts/TestingCodeMonkey.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class TestingCodeMonkey : MonoBehaviour
 {
     Camera cam;
-    GridCodeMonkey grid;
+    GridCodeMonkey<HeatMapGridObject> grid;
     void Start()
     {
         cam = Camera.main;
-        grid = new GridCodeMonkey(7, 5, 10f, new Vector3(20,0));
-        GridCodeMonkey grid2 = new GridCodeMonkey(4, 4, 5f, new Vector3(8, 5));
+        grid = new GridCodeMonkey<HeatMapGridObject>(7, 5, 10f, new Vector3(20,0), (GridCodeMonkey<HeatMapGridObject> g, int x, int y) => new HeatMapGridObject(g, x, y));
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            grid.SetValue(GetMouseWorldPosition(), 56);
+            HeatMapGridObject heatMapGridObject = grid.GetGridObject(GetMouseWorldPosition());
+            if (heatMapGridObject != null)
+            {
+                heatMapGridObject.AddValue(5);
+            }
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+            Debug.Log(grid.GetGridObject(GetMouseWorldPosition()));
         }
     }
 
@@ -37,5 +40,40 @@ public class TestingCodeMonkey : MonoBehaviour
     {
         Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
         return worldPosition;
+    }
+}
+
+public class HeatMapGridObject
+{
+    private const int MIN = 0;
+    private const int MAX = 100;
+
+    private GridCodeMonkey<HeatMapGridObject> grid;
+    private int x;
+    private int y;
+    public int value;
+
+    public HeatMapGridObject(GridCodeMonkey<HeatMapGridObject> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void AddValue(int addValue)
+    {
+        value += addValue;
+        value = Mathf.Clamp(value, MIN, MAX);
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public float GetValueNormalized()
+    {
+        return (float)value / MAX;
+    }
+
+    public override string ToString()
+    {
+        return value.ToString();
     }
 }

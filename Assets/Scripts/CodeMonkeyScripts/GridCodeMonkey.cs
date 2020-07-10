5be70130b240829e62a9fe23ea9ed2bdbf62a89f
Mikzeer <nabil.hallak.nano@gmail.com>
2020-07-10 16:52:29 -0300

    CodeMonkey Grid System Part1

---
+++ Assets/Scripts/CodeMonkeyScripts/GridCodeMonkey.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCodeMonkey
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+    private Vector3 originPosition;
+    private TextMesh[,] debugTextArray;
+
+    public GridCodeMonkey(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        Debug.Log("width " + width + " height " + height);
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.Log("x " + x + " y " + y);
+                debugTextArray[x, y] = CreateWorldText(gridArray[x, y].ToString(), Color.white, null, GetWorldPosition(x, y) + new Vector3(cellSize,cellSize) * 0.5f, 40, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+    
+    private Vector3 GetWorldPosition(int x , int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldposition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldposition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldposition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();            
+        }        
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+    private TextMesh CreateWorldText(string text, Color col, Transform parent = null, Vector3 localPosition = default(Vector3), int fontSize = 40, TextAnchor textAnchor = TextAnchor.UpperLeft, TextAlignment textAlignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
+        GameObject go = new GameObject("World_Text", typeof(TextMesh));
+        Transform trans = go.transform;
+        trans.SetParent(parent, false);
+        trans.localPosition = localPosition;
+        TextMesh textMesh = go.GetComponent<TextMesh>();
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.text = text;
+        textMesh.fontSize = fontSize;
+        textMesh.color = col;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+    }
+
+}
---
+++ Assets/Scripts/CodeMonkeyScripts/TestingCodeMonkey.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingCodeMonkey : MonoBehaviour
+{
+    Camera cam;
+    GridCodeMonkey grid;
+    void Start()
+    {
+        cam = Camera.main;
+        grid = new GridCodeMonkey(7, 5, 10f, new Vector3(20,0));
+        GridCodeMonkey grid2 = new GridCodeMonkey(4, 4, 5f, new Vector3(8, 5));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, cam);
+        vec.z = 0f;
+        return vec;
+    }
+
+    private Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
